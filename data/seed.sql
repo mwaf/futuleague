fd8b0ce6f177c64772a2aa31e55e273f61afd7d7
Jon Krogell <jkro@futurice.com>
2014-01-26 17:51:27 +0100

    Added missing game column to clubs.

--- data/seed.sql
+++ data/seed.sql
@@ -1,16 +1,16 @@
 -- Due to simplistic DB initialization within the program, multi-line statements are not allowed.
 
 -- Games
-insert into games (name) values ("FIFA14");
-insert into games (name) values ("NHL14");
+insert into games values (1, "FIFA14");
+insert into games values (2, "NHL14");
 
 -- FIFA14 Clubs
-insert into clubs (name, league, stars) values ("Bor. Dortmund", "Bundesliga", 5);
-insert into clubs (name, league, stars) values ("FC Bayern Münich", "Bundesliga", 5);
-insert into clubs (name, league, stars) values ("Paris SG", "Ligue 1", 5);
-insert into clubs (name, league, stars) values ("Real Madrid", "La Liga", 5);
-insert into clubs (name, league, stars) values ("Arsenal", "Premier League", 5);
-insert into clubs (name, league, stars) values ("Inter Milan", "Seria A", 5);
+insert into clubs (game, name, league, stars) values (1, "Bor. Dortmund", "Bundesliga", 5);
+insert into clubs (game, name, league, stars) values (1, "FC Bayern Münich", "Bundesliga", 5);
+insert into clubs (game, name, league, stars) values (1, "Paris SG", "Ligue 1", 5);
+insert into clubs (game, name, league, stars) values (1, "Real Madrid", "La Liga", 5);
+insert into clubs (game, name, league, stars) values (1, "Arsenal", "Premier League", 5);
+insert into clubs (game, name, league, stars) values (1, "Inter Milan", "Seria A", 5);
 -- TODO the rest and proper ratings
 
 -- NHL14 Clubs
